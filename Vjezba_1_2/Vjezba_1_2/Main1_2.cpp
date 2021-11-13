3207fc6af19b33db46a594d25cc9e171980f0b90
Matej Copić <matejcopic2802@gmail.com>
2021-11-13 22:54:35 +0100

    vj 1 , zad 2

---
+++ Vjezba_1_2/Vjezba_1_2/Main1_2.cpp
@@ -0,0 +1,26 @@
+#include "funk1_2.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+
+	int m, n;
+	cout << "Unesi dimenziju matrice" << endl;
+	cin >> m;
+	cout << "Unesi dimenziju matrice" << endl;
+	cin >> n;
+	Matrica A = Matrica::input(m, n);
+	int a, b;
+	cout << "Unesi granice a, b" << endl;
+	cin >> a;
+	cin >> b;
+	Matrica B = Matrica::generate(m, n, a, b);
+	Matrica zbroj = Matrica::sum(A, B, m, n);
+	Matrica razlika = Matrica::sub(A, B, m, n);;
+	Matrica umnozak = Matrica::mult(A, B, m, n);
+	print(B, m, n);
+
+	return 0;
+}
---
+++ Vjezba_1_2/Vjezba_1_2/funk1_2.cpp
@@ -0,0 +1,95 @@
+#include "funk1_2.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+Matrica Matrica::input(int redak, int stupac){
+	Matrica matrica;
+	matrica.redak = redak;
+	matrica.stupac = stupac;
+	matrica.m = new float* [redak];
+	for (int i = 0; i < redak; i++) {
+		matrica.m[i] = new float[stupac];
+	}
+	cout << "Unesi elemente u matricu: \n" << endl;
+	for (int i = 0; i < redak; i++){
+		for (int j = 0; j < stupac; j++){
+			cin >> matrica.m[i][j];
+		}
+	}
+	cout << endl;
+	return matrica;
+}
+Matrica Matrica::generate(int redak, int stupac, int a, int b)
+{
+	srand(time(NULL));
+	Matrica matrica;
+	matrica.redak = redak;
+	matrica.stupac = stupac;
+	matrica.m = new float* [redak];
+	for (int i = 0; i < redak; i++) {
+		matrica.m[i] = new float[stupac];
+	}
+	for (int i = 0; i < redak; i++) {
+		for (int j = 0; j < stupac; j++) {
+			matrica.m[i][j] = a + (rand() % (b - a + 1));
+		}
+	}
+	return matrica;
+}
+Matrica Matrica::sum(Matrica matrica1, Matrica matrica2, int redak, int stupac){
+	Matrica matrica;
+	matrica.redak = redak;
+	matrica.stupac = stupac;
+	matrica.m = new float* [redak];
+	for (int i = 0; i < redak; i++) {
+		matrica.m[i] = new float[stupac];
+	}
+	for (int i = 0; i < redak; ++i) {
+		for (int j = 0; j < stupac; ++j) {
+			matrica.m[i][j] = (matrica1.m[i][j] + matrica2.m[i][j]);
+		}
+	}
+	return matrica;
+}
+Matrica Matrica::sub(Matrica matrica1, Matrica matrica2, int redak, int stupac){
+	Matrica matrica;
+	matrica.redak = redak;
+	matrica.stupac = stupac;
+	matrica.m = new float* [redak];
+	for (int i = 0; i < redak; i++) {
+		matrica.m[i] = new float[stupac];
+	}
+	for (int i = 0; i < redak; i++) {
+		for (int j = 0; j < stupac; j++) {
+			matrica.m[i][j] = (matrica1.m[i][j] - matrica2.m[i][j]);
+		}
+	}
+	return matrica;
+}
+Matrica Matrica::mult(Matrica matrica1, Matrica matrica2, int redak, int stupac)
+{
+	Matrica matrica;
+	matrica.redak = redak;
+	matrica.stupac = stupac;
+	matrica.m = new float* [redak];
+	for (int i = 0; i < redak; i++) {
+		matrica.m[i] = new float[stupac];
+	}
+	for (int i = 0; i < redak; i++) {
+		for (int j = 0; j < stupac; j++) {
+			matrica.m[i][j] = (matrica1.m[i][j] * matrica2.m[i][j]);
+		}
+	}
+	return matrica;
+}
+void print(Matrica matrica, int redak, int stupac)
+{
+	for (int i = 0; i < redak; i++) {
+		for (int j = 0; j < stupac; j++) {
+			cout << fixed << setprecision(4) << right << matrica.m[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
